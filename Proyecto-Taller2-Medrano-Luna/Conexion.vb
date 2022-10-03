f6b9b454633ee8e865882bae4fb92513af8051e4
Franco Medrano <franconicolas379@gmail.com>
2022-10-03 09:51:27 -0300

    conexion base de datos
    se realiza la conexion a la base de datos

---
+++ Proyecto-Taller2-Medrano-Luna/Conexion.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Data.Sql
+Imports System.Data.SqlClient
+
+Module Conexion
+    Public connection As New SqlConnection("Data Source=DESKTOP-GQ798UG\SQLEXPRESS_INST;Initial Catalog=ProyectoTaller;Integrated Security=True")
+    Public comando As New SqlCommand
+
+
+    Sub abrir()
+        If connection.State = 0 Then
+            connection.Open()
+        End If
+    End Sub
+
+    Sub cerrar()
+        If connection.State = 1 Then
+            connection.Close()
+        End If
+    End Sub
+End Module
