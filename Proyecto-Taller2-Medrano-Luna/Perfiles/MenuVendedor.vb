dd91cde13a99094805ec1c7bf85385777b848d85
santiilunaa <santiago.luna3101@gmail.com>
2022-10-30 21:03:23 -0300

    agregado de hora en los demás perfiles

--- Proyecto-Taller2-Medrano-Luna/Perfiles/MenuVendedor.vb
+++ Proyecto-Taller2-Medrano-Luna/Perfiles/MenuVendedor.vb
@@ -45,4 +45,13 @@
         btnVentas.BackColor = Color.White
     End Sub
 
+    Private Sub MenuVendedor_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Timer1.Enabled = True
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        lblhora.Text = DateTime.Now.ToString("hh:mm:ss")
+        lblFecha.Text = DateTime.Now.ToLongDateString()
+    End Sub
 End Class
